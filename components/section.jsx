d2901ffef6359b813f5f3de57691a6799669bad6
walterlee <walter.lee9@gmail.com>
2019-03-18 13:33:10 -0500

    css cleanup

--- components/section.jsx
+++ components/section.jsx
@@ -4,26 +4,16 @@ class Section extends Component {
 
     render() {
         return (
-            <section id="about" className="about-section text-center">
-                {/* <div className="container">
-
-                    <div className="row">
-
-                        <div className="col-lg-8 mx-auto">
-                            <h2 className="text-white mb-4">About Me</h2>
-                            <p className="text-white-50">Welcome to my Website!</p>
-                        </div>
-                    </div>
-
-                    <img src="bootstrap/img/Walt Image.jpg" className="img-fluid" alt="" />
-                </div> */}
+            <section id="about" className="about-section about-style text-center">
                 <div className="heroform">
-                    <div className="hero-left">
+                    <div className="hero">
                         <img src="bootstrap/img/walt.jpg" height="400" width="250" />
+
                     </div>
-                    <div className="hero-right">
-                        <h3 className="text-uppercase">Nashville, TN</h3>
+                    <div>
+                        <a href="#projects" className="btn btn-style js-scroll-trigger"></a>
                     </div>
+
                 </div>
             </section>
         );
