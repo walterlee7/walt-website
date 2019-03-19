0fbf0a98bb41958c6a3cacea73f2a00c7a5a6333
walterlee <walter.lee9@gmail.com>
2019-03-19 13:15:10 -0500

    styling on about section

--- components/section.jsx
+++ components/section.jsx
@@ -6,20 +6,19 @@ class Section extends Component {
     render() {
         return (
             <section id="about" className="about-section about-style text-center">
-                {/* <div className="heroform">
+                <div className="heroform">
                     <div className="hero">
-                        <img src="bootstrap/img/walt.jpg" height="400" width="250" />
-                        <p className="p-style">Welcome to my Website!</p>
+                        <img src="bootstrap/img/Walt Image.jpg" height="200" width="200" />
+                        {/* <p className="p-style">Welcome to my Website!</p> */}
                     </div>
-                </div> */}
+                </div>
 
                 <canvas id='c' style={{
-
                     top: + '0',
                     left: + '0',
+                }}>
 
-
-                }}></canvas>
+                </canvas>
 
             </section >
         );
