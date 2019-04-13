7ca1714409dcdc3e95bbd1fe3b4059da8ee4128c
walterlee <walter.lee9@gmail.com>
2019-04-13 16:53:01 -0500

    Updates

---
+++ precache-manifest.f2100ad8a68bc811de1c2ebf828474fb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a8f2ea018491d660842",
+    "url": "/static/js/main.f3cc9fd8.chunk.js"
+  },
+  {
+    "revision": "9d1303fbbc4d78d09aa2",
+    "url": "/static/js/2.00f4a7c7.chunk.js"
+  },
+  {
+    "revision": "37301edcc1f78aa52910a9c640790dca",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/walt-website/precache-manifest.70b7f02b54510a2acddeefd30965c977.js"
+  "/precache-manifest.f2100ad8a68bc811de1c2ebf828474fb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/walt-website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
